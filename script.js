67a0288b2b6f19cccdd9dacb7dc78f0f7e740e77
natasharrison <natasharrison@gmail.com>
2021-04-22 18:50:25 -0700

    api key added

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ var cityInputEl = document.querySelector("#city");
 var getWeather = function (city) {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=65c9a13b026d55f0a4cc05734545fc57`).then(function(response) {
         console.log(response);
-});
+        return response.json()
+})
+        .then(function(data){
+            console.log(data);
+        }) 
 };
 
 // handle form submission of city search 

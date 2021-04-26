f215122cbf97169196a8f652857672d702a15b12
natasharrison <natasharrison@gmail.com>
2021-04-25 17:10:54 -0700

    current weather data

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 var formEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#city");
 
+var currentCityEl = document.querySelector("#current-city");
+
 // api fetch 
 var getWeather = function (city) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=65c9a13b026d55f0a4cc05734545fc57`).then(function(response) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=65c9a13b026d55f0a4cc05734545fc57`).then(function(response) {
         console.log(response);
         return response.json()
 })
         .then(function(data){
             console.log(data);
-            // displayWeather()
+            displayWeather(data);
         }) 
 };
 
@@ -29,10 +31,18 @@ var formSubmitHandler = function(event){
 };
 
 // display current weather 
-// var displayWeather = function(weather, searchTerm) {
-//     console.log(weather);
-//     console.log(searchTerm);
-// };
+var displayWeather = function(data){
+    currentCityEl.textContent = data.name;
+    document.getElementById("icon").innerHTML = data.weather[0].icon;
+    document.getElementById("temp").innerHTML = data.main.temp;
+    document.getElementById("wind").innerHTML = data.wind.speed;
+    document.getElementById("humidity").innerHTML = data.main.humidity;
+   
+};
+
+// get the image from the website by creating image variable
+// var imageEl = 
+// Image = "src" "http://"
 
 
 formEl.addEventListener("submit", formSubmitHandler);
